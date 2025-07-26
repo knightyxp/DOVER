201ddce71fa98e3f7194728e15147071b3d56d64
Xiangpeng Yang <knightyxp@gmail.com>
2025-07-26 18:14:52 +1000

    rename

--- evaluate_senorita.sh
+++ evaluate_senorita.sh
@@ -1,4 +1,4 @@
 export CUDA_VISIBLE_DEVICES=0,1,2,3
 python evaluate_a_set_of_videos.py \
     -in /scratch3/yan204/yxp/Senorita/local_style_transfer_upload \
-    -out /scratch3/yan204/yxp/Senorita/dover_predictions/local_style_transfer_upload.csv \
+    -out /scratch3/yan204/yxp/Senorita/dover_predictions/local_style_transfer_dover_score.csv \
